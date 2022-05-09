15215dcdb09cc83d48f7a9985e7caa3e23c78047
andresguastavino <andresguastavino@gmail.com>
2022-05-09 15:53:53 -0300

    dotenv requirement taken

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-require('dotenv/config');
 const express = require('express');
 
 const app = express();
-const PORT = process.env.port || 5000;
+const PORT = process.env.port || 3000;
 
 const outboundSMSHandler = require('./lib/outboundSMSHandler');
 
